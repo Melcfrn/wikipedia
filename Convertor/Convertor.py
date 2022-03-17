a348943eff2c4b15b3669a680d92cac8da503cc3
Melcfrn <clement.91m@gmail.com>
2022-03-17 16:58:15 +0100

    creation de l'ensemble des fichiers nécessaires au convertor

---
+++ Convertor/Convertor.py
@@ -0,0 +1,13 @@
+
+
+class Convertor():
+    def __init__(self):
+
+
+    def toStringTable(self, table) :
+        """
+        """
+
+    def toStringTables(self, tables) :
+        """
+        """
---
+++ TemplateHTML/Balise/Balise.py
@@ -0,0 +1,6 @@
+import TemplateHTML.Grid.Grid
+import TemplateHTML.Visitor.Visitor
+
+from abc import ABC, abstractmethod
+
+class Balise():
